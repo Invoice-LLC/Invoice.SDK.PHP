820db0be014b3f390fbda9dbe25fb40e00550969
Kirill Kuznetsov <trojan4ikdayz@gmail.com>
2020-01-28 13:34:11 +0500

    New: Example project

---
+++ example/PseudoProcessing.php
@@ -0,0 +1,117 @@
+<?php
+require_once "../sdk/RestClient.php";
+require_once "../sdk/GET_TERMINAL.php";
+require_once "../sdk/CREATE_TERMINAL.php";
+require_once "../sdk/CREATE_PAYMENT.php";
+require_once "../sdk/CREATE_REFUND.php";
+require_once "../sdk/CLOSE_PAYMENT.php";
+require_once "../sdk/common/ITEM.php";
+
+class PseudoProcessing
+{
+    const shopName = "Название магазина";
+    const shopDescription = "Описание магазина";
+    const terminalType = "dynamical";
+    const defaultPrice = 0;
+
+    private $restClient;
+    private $paymentInfo;
+    private $terminalInfo;
+    private $refundInfo;
+
+    public function init($login, $apiKey, $alias)
+    {
+        if($this->terminalInfo == null)
+        {
+            $this->restClient = new RestClient($login, $apiKey);
+            $get_terminal = new GET_TERMINAL($alias);
+
+            $this->terminalInfo = $this->restClient->GetTerminal($get_terminal);
+            if($this->terminalInfo or $this->terminalInfo->id == null)
+            {
+                $create_terminal = new CREATE_TERMINAL();
+
+                $create_terminal->alias = $alias;
+                $create_terminal->name = self::shopName;
+                $create_terminal->description = self::shopDescription;
+                $create_terminal->type = self::terminalType;
+                $create_terminal->defaultPrice = self::defaultPrice;
+
+                $this->restClient->CreateTerminal($create_terminal);
+            }
+
+        }
+    }
+
+    public function onPay(array $items)
+    {
+        $create_payment = new CREATE_PAYMENT();
+
+        $create_payment->receipt = $items;
+
+        $settings = new SETTINGS();
+        $settings->terminal_id = $this->terminalInfo->id;
+
+        $this->paymentInfo = $this->restClient->CreatePayment($create_payment);
+
+        if($this->paymentInfo == null or $this->paymentInfo->id == null)
+        {
+            return false;
+        }else
+        {
+            return true;
+        }
+    }
+
+    public function onCancel($orderId)
+    {
+        $close_payment = new CLOSE_PAYMENT($orderId);
+
+        $this->restClient->ClosePayment($close_payment);
+    }
+
+    public function onRefund($orderID, array $items, string $reason, int $amount)
+    {
+        $create_refund = new CREATE_REFUND();
+        $create_refund->receipt = $items;
+        $create_refund->id = $orderID;
+
+        $refund_info = new REFUND_INFO();
+        $refund_info->reason = $reason;
+        $refund_info->amount = $amount;
+
+        $create_refund->refund = $refund_info;
+
+        $this->refundInfo = $this->restClient->CreateRefund($create_refund);
+
+        if($this->refundInfo->error == null)
+        {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public function getStatus(string $orderId)
+    {
+        $get_payment = new GET_PAYMENT($orderId);
+        $this->paymentInfo = $this->restClient->GetPayment($get_payment);
+
+        return $this->paymentInfo->status;
+    }
+
+    public function getPayment()
+    {
+        return $this->paymentInfo;
+    }
+
+    public function getTerminal()
+    {
+        return $this->terminalInfo;
+    }
+
+    public function getRefund()
+    {
+        return $this->refundInfo;
+    }
+}
--- example/index.php
+++ example/index.php
@@ -1,7 +1,76 @@
 <?php
+require_once "PseudoProcessing.php";
+
+$action = $_GET['action'];
+
+$processing = new PseudoProcessing();
+
+$processing->init("demo","1526fec01b5d11f4df4f2160627ce351","1:1");
+
+$item1 = new ITEM();
+$item1->name = "Суп";
+$item1->price = 10;
+$item1->discount = 0;
+$item1->quantity = 2;
+$item1->resultPrice = 20;
+
+$item2 = new ITEM();
+$item2->name = "Кефир";
+$item2->price = 1000;
+$item2->discount = 10;
+$item2->quantity = 1;
+$item2->resultPrice = 990;
+
+$items = [
+    $item1,
+    $item2
+];
+
+switch ($action) {
+    case "pay":
+        $pay = $processing->onPay($items);
+
+        if($pay)
+        {
+            echo "Платеж оформлен: ".$processing->getPayment()->id;
+        } else {
+            echo "Ошибка платежа";
+        }
+        break;
+    case "cancel":
+        if(empty($_GET['id']))
+            break;
+
+        $processing->onCancel($_GET['id']);
+        echo "Платеж отменен";
+        break;
+    case "status":
+        if(empty($_GET['id']))
+            break;
+
+        $status = $processing->getStatus($_GET['id']);
+        echo $status;
+        break;
+    case "refund":
+        if(empty($_GET['id']))
+            break;
+
+        $refund = $processing->onRefund($_GET['id'] ,$items, "Муха в супе",20);
+
+        if($refund) {
+            echo "Возврат оформлен";
+        }else {
+            echo "Ошибка возврата";
+        }
+        break;
+
+    default:
+        echo $processing->getTerminal()->id;
+        echo "<br>";
+        echo $processing->getTerminal()->link;
+        break;
+}
+
+
 ?>
 
-<html>
-<head></head>
-<body
-</html>
